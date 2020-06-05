65395fb520e323cd251927103c0d5e08d34b8b32
Alejandro Rios <luisrios.lar@gmail.com>
2020-06-05 00:20:25 -0500

    v1.1

--- read.py
+++ read.py
@@ -1,22 +1,11 @@
 import imaplib
-import base64
-import os
 import email
+import DownloadAttachments
 
-email_user = 'luisrios.lar@gmail.com'
-email_pass = '939030e99'
-mail = imaplib.IMAP4_SSL("imap.gmail.com",993)
-mail.login(email_user, email_pass)
-mail.select('Inbox')
-type, data = mail.search(None, '(SUBJECT "EI")')
-idlist = data[0].split()
+server = "imap.gmail.com"
+user = "luisrios.lar@gmail.com"
+password = "939030e99"
+outputdir = "C:/Users/luisr/Desktop/Documentos importantes/IntegrationErrors/Documents"
 
-for num in idlist:
-    typ, data = mail.fetch(num, '(RFC822)' )
-    raw_email = data[0][1]
-    raw_email_string = raw_email.decode('utf-8')
-    email_message = email.message_from_string(raw_email_string)
-    print('From: '+email_message['from'])
-    print('body: '+str(email_message.get_payload()[0]))
-    atta = email.message_from_file(open('Data.xlsx'))
-    attachment = atta.get_payload()[1]
+connection = [server,user,password,outputdir]
+DownloadAttachments.FetchEmail(connection)
